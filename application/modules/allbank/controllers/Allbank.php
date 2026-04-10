39bd0716665d607588aa6fc662f6596afbdf0f73
rhonbhorj <rhonbhorj@gmail.com>
2026-04-10 09:59:12 +0800

    update postbak logs

--- application/modules/allbank/controllers/Allbank.php
+++ application/modules/allbank/controllers/Allbank.php
@@ -15,7 +15,7 @@ class Allbank extends REST_Controller
     {
         parent::__construct();
         date_default_timezone_set('Asia/Manila');
-        $this->load->model('Postback_model', 'modelrepo');
+        $this->load->model('Allbank_model', 'modelrepo');
         $this->load->helper('allbank_helper');
 
     }
@@ -36,9 +36,20 @@ class Allbank extends REST_Controller
     public function instapay_post()
     {   
 
-        $data['status'] = false;
-        $data['message'] = 'instapay';
-        $this->response($data, Rest_Controller::HTTP_FORBIDDEN);
+        $pData = file_get_contents('php://input');
+
+        $encData = json_encode($pData);
+        $response['callback_data'] = json_decode($encData, true).$this->uri->uri_string();
+        $c_log= $this->modelrepo->callback_logs($response);
+        
+        $dateToday = date('Y-m-d\TH:i:s.vP');
+
+        $resp['payment_channel'] = 'AllBank';
+        $resp['payment_datetime'] = $dateToday;
+        $resp['status'] = 'Successful';
+        $resp['message'] = 'Payment Received';
+      
+        $this->response($resp, Rest_Controller::HTTP_OK);
     }
         public function pesonet_post()
     {   
@@ -56,6 +67,9 @@ class Allbank extends REST_Controller
         $data['message'] = 'p2m';
         $this->response($data, Rest_Controller::HTTP_FORBIDDEN);
     }
+
+
+
     
     public function payment_get()
     {
